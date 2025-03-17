a7c28d4d226d9ada707a8502f200b2704087bc86
DavidHerreros <david.herreroscalero@gmail.com>
2025-03-17 11:27:54 +0100

    Fix channel order

--- roodmus/__init__.py
+++ roodmus/__init__.py
@@ -104,7 +104,7 @@ class Plugin(pwem.Plugin):
                                    drv <= nvidiaDriverVer]
             cudaVersion = max(compatible_versions)
             commands += (
-                f"conda create -n roodmus-{V1} -c conda-forge -c nvidia/label/cuda-{cudaVersion} python=3.10 "
+                f"conda create -n roodmus-{V1} -c nvidia/label/cuda-{cudaVersion} -c conda-forge -c anaconda python=3.10 "
                 f"fftw cuda={cudaVersion} gcc=11.4.0 cxx-compiler -y && ")
             commands += f"export LD_LIBRARY_PATH=$CONDA_PREFIX/lib/:$LD_LIBRARY_PATH && "
             commands += f"conda activate roodmus-{V1} && "
