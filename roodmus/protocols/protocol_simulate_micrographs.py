b470d1ad5d7f18d4dfe6ef5a5eb69a2a6a719e5f
DavidHerreros <david.herreroscalero@gmail.com>
2026-01-15 09:30:09 +0100

    Set stddev to a small value to force delta distributions

--- roodmus/protocols/protocol_simulate_micrographs.py
+++ roodmus/protocols/protocol_simulate_micrographs.py
@@ -213,7 +213,7 @@ class ProtSimulateMicrographs(EMProtocol):
                 f"--centre_y {pixelSize * centreY} --centre_z {centreZ} --cuboid_length_x {pixelSize * nX} "
                 f"--cuboid_length_y {pixelSize * nY} --cuboid_length_z {iceThickness} --tqdm "
                 f"--nproc {self.numberOfThreads.get()} --electrons_per_angstrom {self.dose.get()} "
-                f"--c_10 {defocusAverage} ")
+                f"--c_10 {defocusAverage} --c_10_stddev 1e-6")
                 # f"--model {self._micModel[self.micModel.get()]}")  # FIXME: Currently a bug in Roodmus, to be added when fixed
 
         if self.usesGpu():
